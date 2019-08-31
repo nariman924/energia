678b9b69808f4ca284d77a3384602ca4eeee6a00
nariman <n.ablyakimov@globus-ltd.com>
2019-08-31 18:11:17 +0300

    Натягиваем вёрстку

--- frontend/views/layouts/_clear.php
+++ frontend/views/layouts/_clear.php
@@ -14,11 +14,32 @@ use yii\helpers\Html;
     <title><?php echo Html::encode($this->title) ?></title>
     <?php $this->head() ?>
     <?php echo Html::csrfMetaTags() ?>
+
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap4/bootstrap.min.css">
+    <link href="/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/animate.css">
+    <link rel="stylesheet" type="text/css" href="/plugins/slick-1.8.0/slick.css">
+    <link rel="stylesheet" type="text/css" href="/css/main_styles.css">
+    <link rel="stylesheet" type="text/css" href="/css/responsive.css">
 </head>
 <body>
 <?php $this->beginBody() ?>
     <?php echo $content ?>
 <?php $this->endBody() ?>
+<script src="/js/jquery-3.3.1.min.js"></script>
+<script src="/css/bootstrap4/popper.js"></script>
+<script src="/css/bootstrap4/bootstrap.min.js"></script>
+<script src="/plugins/greensock/TweenMax.min.js"></script>
+<script src="/plugins/greensock/TimelineMax.min.js"></script>
+<script src="/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="/plugins/greensock/animation.gsap.min.js"></script>
+<script src="/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="/plugins/slick-1.8.0/slick.js"></script>
+<script src="/plugins/easing/easing.js"></script>
+<script src="/js/custom.js"></script>
 </body>
 </html>
 <?php $this->endPage() ?>
